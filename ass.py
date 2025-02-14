f8068e91cf534f0645702bfdf7f4d19ab9e20bbf
hsenmrad <husseinmrad.business@gmail.com>
2025-02-14 06:37:14 +0200

    Ex2

--- ass.py
+++ ass.py
@@ -13,3 +13,10 @@ else :
 print("The ticket price is :$" + str(price));
 # End of Exercise 1
       
+# Exercise 2
+number=int(input("Enter a number: "))
+if number%2==0:
+ print("The number is even.")
+else:
+    print("The number is odd.")
+    
