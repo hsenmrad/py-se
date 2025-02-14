72f6fa1222b17a55f167e6327e1b9163d7d98821
hsenmrad <husseinmrad.business@gmail.com>
2025-02-14 06:33:20 +0200

    Ex1

---
+++ ass.py
@@ -0,0 +1,15 @@
+# Exercise 1
+age=int(input("Enter your age : "))
+
+if age<3 :
+    price =0
+elif age<=12:
+    price =7;
+elif age<=40:
+    price= 10;
+else :
+    price =12;
+    
+print("The ticket price is :$" + str(price));
+# End of Exercise 1
+      
